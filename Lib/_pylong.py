d1a9cb461c0c4fca18f866d751600327f8f57527
Sergey B Kirpichev <skirpichev@gmail.com>
2023-08-28 11:17:34 +0300

    XXX move k_mul() implementation to Lib/_pylong.py

--- Lib/_pylong.py
+++ Lib/_pylong.py
@@ -283,3 +283,19 @@ def int_divmod(a, b):
         return ~q, b + ~r
     else:
         return _divmod_pos(a, b)
+
+
+def k_mul(x, y):
+    n = max(x.bit_length(), y.bit_length()) // 2
+    x0 = x >> n
+    x -= x0 << n
+    if x == y:
+        y0 = x0
+        y = x
+    else:
+        y0 = y >> n
+        y -= y0 << n
+    s1 = x0 * y0
+    s2 = x * y
+    s3 = (x0 + x) * (y0 + y) - s1 - s2
+    return (s1 << (n << 1)) + (s3 << n) + s2
