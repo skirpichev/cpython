737293b3c607a715e865c8f58de61a361f41c731
Sergey B Kirpichev <skirpichev@gmail.com>
2024-05-11 12:06:42 +0300

    Use floats for signed zeros in the real part of repr(complex)
    >>> complex(-0.0, 1)  # was (-0+1j)
    (-0.0+1j)
    
    This doesn't break complex(str(x)) == x invariant as we
    had support for parsing signed 0.0's here before.

--- Lib/test/test_complex.py
+++ Lib/test/test_complex.py
@@ -835,14 +835,14 @@ class ComplexTest(ComplexesAreIdenticalMixin, unittest.TestCase):
             test_fn(str(v), expected)
 
         test(complex(0., 1.),   "1j")
-        test(complex(-0., 1.),  "(-0+1j)")
+        test(complex(-0., 1.),  "(-0.0+1j)")
         test(complex(0., -1.),  "-1j")
-        test(complex(-0., -1.), "(-0-1j)")
+        test(complex(-0., -1.), "(-0.0-1j)")
 
         test(complex(0., 0.),   "0j")
         test(complex(0., -0.),  "-0j")
-        test(complex(-0., 0.),  "(-0+0j)")
-        test(complex(-0., -0.), "(-0-0j)")
+        test(complex(-0., 0.),  "(-0.0+0j)")
+        test(complex(-0., -0.), "(-0.0-0j)")
 
     def test_pos(self):
         self.assertEqual(+(1+6j), 1+6j)
